182e2f239f3670d068db20d00c6b51f3d5362097
Igor Ramon <ramon.riotinto@gmail.com>
2024-02-07 17:08:09 -0300

    layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Head from "next/head";
+
+export default function Layout({ children }: any) {
+    return (
+        <>
+            <Head>
+                <link rel="shortcut icon" href="/images/favicon.ico"></link>
+                <title>Poke-Next</title>
+            </Head>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+        </>
+    );
+}
