9d872c57a962af36654641f801abf7b3ab19f65a
Igor Ramon <ramon.riotinto@gmail.com>
2024-02-10 10:28:35 -0300

    css about

---
+++ src/pages/about.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import styles from "../styles/About.module.css";
+
+export default function About() {
+    return (
+        <>
+            <div className={styles.about}>
+                <h1>Sobre o Projeto</h1>
+                <p>Aprendendo Next com Matheus Battist, canal Hora de Codar</p>
+                <Image
+                    src="/images/charizard.png"
+                    width="300"
+                    height="300"
+                    alt="Charizard"
+                />
+            </div>
+        </>
+    );
+}
